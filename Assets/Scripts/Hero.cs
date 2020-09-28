ce55819f3090b621f4473fbf740dbfb8758f9cae
Ryan Canuel <ryancanuel@hotmail.com>
2020-09-28 15:41:47 -0400

    Implemented Hero Movement

--- Assets/Scripts/Hero.cs
+++ Assets/Scripts/Hero.cs
@@ -4,15 +4,39 @@ using UnityEngine;
 
 public class Hero : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+    static public Hero S; //Singleton
+
+    [Header("Set in Inspector")]
+
+    //Fields control the moevement of the ship
+    public float speed = 30;
+    public float rollMult = -45;
+    public float pitchMult = 30;
+
+    [Header("Set Dynamically")]
+    public float shieldLevel = 1;
+
+    void Awake() {
+        if(S == null) {
+            S = this;
+        } else {
+            Debug.LogError("Hero.Awake() - Attempted to assign second Hero.S!");
+        }
     }
 
     // Update is called once per frame
-    void Update()
-    {
-        
+    void Update() {
+        //Pull in info from Input class
+        float xAxis = Input.GetAxis("Horizontal");
+        float yAxis = Input.GetAxis("Vertical");
+
+        //Change transform.poition based on axes
+        Vector3 pos = transform.position;
+        pos.x += xAxis * speed * Time.deltaTime;
+        pos.y += yAxis * speed * Time.deltaTime;
+        transform.position = pos;
+
+        //Rotate ship to make it feel dynamic
+        transform.rotation = Quaternion.Euler(yAxis * pitchMult, xAxis * rollMult, 0);
     }
 }
