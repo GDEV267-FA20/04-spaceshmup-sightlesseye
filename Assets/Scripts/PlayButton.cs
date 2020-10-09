985a803e34aaf60928c89db6f1a66d3af2e03e5f
Ryan Canuel <ryancanuel@hotmail.com>
2020-10-08 22:38:01 -0400

    Added Main Menu and Play Button

---
+++ Assets/Scripts/PlayButton.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement; // <== remember to use this anytime you use Scenes
+
+public class PlayButton : MonoBehaviour {
+    
+    public void PlayGame() {
+        //SceneManager deals with the scenes 
+        //GetActiveScene gets current scene
+        //buildIndex is the index of where the scene is in Unity's scene queue
+        //SO
+        //This code loads the next scene.
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+}
